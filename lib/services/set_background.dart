5a2e68a0a0444835740831ca0690a2309efdfda2
Leon Edlund <Leone.edlund@gmail.com>
2024-08-10 23:01:39 +0200

    change nighttime clear sky color

--- lib/services/set_background.dart
+++ lib/services/set_background.dart
@@ -13,7 +13,7 @@ class Background {
 Background setBackground(int weatherCode, double sunset, double sunrise) {
   const Color badWeatherNight = Color.fromARGB(255, 64, 64, 64);
   const Color badWeatherDay = Colors.grey;
-  const Color clearSkyNight = Color.fromARGB(255, 2, 79, 115);
+  const Color clearSkyNight = Color.fromARGB(255, 0, 49, 71);
   const Color clearSkyDay = Color.fromARGB(255, 2, 141, 205);
   const Color defaultColor = Colors.black;
 
